b0a6dbf9871b189b53a6be3bcb540d58b5fb4bde
rokysf <135695631+rokysf@users.noreply.github.com>
2023-10-19 12:44:19 +0200

    Add files via upload

--- S04T02N01MonsIglesiasJordi/src/main/java/cat/itacademy/barcelonactiva/monsiglesias/jordi/s04/t02/n01/model/domain/Fruita.java
+++ S04T02N01MonsIglesiasJordi/src/main/java/cat/itacademy/barcelonactiva/monsiglesias/jordi/s04/t02/n01/model/domain/Fruita.java
@@ -1,14 +1,29 @@
 package cat.itacademy.barcelonactiva.monsiglesias.jordi.s04.t02.n01.model.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Fruita {
-	private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
 	private String nom;
+	@Column(nullable = false)
 	private int quantitatQuilos;
 	
-	
 }
+
