e00f95dc17ee98ed22c6d3c764172c7427eec364
rokysf <135695631+rokysf@users.noreply.github.com>
2023-10-19 11:57:38 +0200

    Add files via upload

---
+++ S04T02N01MonsIglesiasJordi/src/main/java/cat/itacademy/barcelonactiva/monsiglesias/jordi/s04/t02/n01/model/domain/Fruita.java
@@ -0,0 +1,14 @@
+package cat.itacademy.barcelonactiva.monsiglesias.jordi.s04.t02.n01.model.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Fruita {
+	private int id;
+	private String nom;
+	private int quantitatQuilos;
+	
+	
+}
